200722cd19846bf0b4ae139c96351172d1a184bb
Alex Howley <howley.alex@gmail.com>
2023-11-26 01:22:49 -0600

    Remove unused validation from tracks

--- src/validation/track.ts
+++ src/validation/track.ts
@@ -1,17 +1,4 @@
-import configure from "knex";
-import knexfile from "../../knexfile.js";
-import { body } from "express-validator";
-import { errorMessages } from "./config.js";
 import { requiredValidator } from "./common.js";
-
-const knex = configure(knexfile);
-
-const validateTrackExists = async (trackId: number) => {
-  const tracks = await knex("track").where({ id: trackId });
-  if (!tracks.length) {
-    throw new Error(errorMessages.trackNotFound(trackId));
-  }
-};
 
 export const postValidators = () => [
   requiredValidator("name"),
