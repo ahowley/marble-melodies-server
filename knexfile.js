f8c3c2da624cfca6660fb827d1aaea18dc4d8a78
Alex Howley <howley.alex@gmail.com>
2023-11-27 12:53:22 -0600

    Correct knexfile config for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,17 @@
 import "dotenv/config";
 
+const connectionSettings = {
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  charset: "utf8",
+};
+if (process.env.DB_PORT) connectionSettings.port = process.env.DB_PORT;
+
 export default {
   client: "mysql2",
-  connection: process.env.DATABASE_URL || {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    charset: "utf8",
-  },
+  connection: connectionSettings,
   seeds: {
     directory: "./seeds",
   },
